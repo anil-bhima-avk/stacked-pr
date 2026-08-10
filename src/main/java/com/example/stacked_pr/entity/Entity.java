bd68003cd3754fcf51798ea5323bf939b0ca2111
Anil Bhima <anil.bhima@avktechinc.com>
2026-08-10 20:06:33 +0530

    entity commit2

--- src/main/java/com/example/stacked_pr/entity/Entity.java
+++ src/main/java/com/example/stacked_pr/entity/Entity.java
@@ -3,5 +3,17 @@ package com.example.stacked_pr.entity;
 public class Entity {
 
     private String name;
+   // fill in other fields as needed
 
+    public Entity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
